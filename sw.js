b23f8604e114e1f391ae5c4554e9d700b7e35d03
KsawerUwU <valeriya.kolesnikowa2017@yandex.ru>
2025-12-08 16:38:11 +0300

    qr

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
-// Простой оффлайн-кэш для статического хостинга
-const CACHE = "habify-v2";
+// PWA-кэш, чтобы всё (включая QR-библиотеки) работало оффлайн
+const CACHE = "habify-v3";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./qrcode.min.js",
+  "./jsqr.min.js"
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
@@ -25,12 +29,14 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
   const req = e.request;
   e.respondWith(
-    caches.match(req).then(hit => hit || fetch(req).then(res => {
-      if (req.method === "GET" && new URL(req.url).origin === location.origin) {
-        const resClone = res.clone();
-        caches.open(CACHE).then(c => c.put(req, resClone));
-      }
-      return res;
-    }).catch(()=> caches.match("./index.html")))
+    caches.match(req).then(hit =>
+      hit || fetch(req).then(res => {
+        if(req.method==="GET"){
+          const clone = res.clone();
+          caches.open(CACHE).then(c=>c.put(req,clone));
+        }
+        return res;
+      }).catch(()=>caches.match("./index.html"))
+    )
   );
 });
